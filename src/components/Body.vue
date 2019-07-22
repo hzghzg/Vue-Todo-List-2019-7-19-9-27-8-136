78f713b7ee33f3a5149150c9aa07df2711bcb30e
DARYL HUANG (ITA-ISDC-ISD-OOCLL/ZHA) <HUANGDA7@OOCL.COM>
2019-07-22 14:14:15 +0800

    add InputBar

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -1,12 +1,18 @@
 <template>
-  <div id="Body"></div>
+  <div id="Body">
+      <InputBar></InputBar>
+      <ListContainer></ListContainer>
+  </div>
 </template>
 
 <script>
-
+import InputBar from "../components/InputBar.vue";
+import ListContainer from "../components/ListContainer.vue";
 export default {
   name: 'Body',
   components: {
+      InputBar,
+      ListContainer
   }
 }
 </script>
---
+++ src/components/InputBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="InputBar">
+      <label><input type="text"></label>
+      <input type="button" value="Add">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputBar',
+  components: {
+  }
+}
+</script>
